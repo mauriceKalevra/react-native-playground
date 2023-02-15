8dad7203ffaf42ec6642cf954898fe342a3f0481
Maurice <Mori.r@web.de>
2023-02-15 18:37:44 +0100

    add jpg

--- playground/App.js
+++ playground/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+     <Image 
+     style={{width: 200}}
+     source={require('./assets/themes/stronghold.jpg')}
+    />
+    
     {/* Entry Screen */}
     <View style={styles.taskWrapper}>
+
+   
       <Text style={styles.sectionTitle}>Welcome to the Stronghold Crusader Soundboard</Text>
       <Text style={styles.undersection}>by Maurice Kalevra</Text>
     </View>
@@ -23,6 +30,7 @@ const styles = StyleSheet.create({
   taskWrapper: {
     paddingTop: 250,
     paddingHorizontal: 20,
+    
   },
   sectionTitle: {},
   undersection: {
